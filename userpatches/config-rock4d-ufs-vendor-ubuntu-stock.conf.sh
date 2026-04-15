cb977183a6083c885bd41609636bf2c7d956a11b
xin <lighthx@gmail.com>
2026-04-15 10:27:04 +0800

    Add ROCK 4D vendor Ubuntu build workflows

---
+++ userpatches/config-rock4d-ufs-vendor-ubuntu-stock.conf.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+BOARD="radxa-rock-4d"
+BRANCH="vendor"
+RELEASE="noble"
+
+BUILD_DESKTOP="no"
+BUILD_MINIMAL="yes"
+NETWORKING_STACK="network-manager"
+KERNEL_CONFIGURE="no"
+UBOOT_CONFIGURE="no"
+
+CPUFREQUTILS_ENABLE="true"
+GOVERNOR="performance"
+
+EXTRAWIFI="yes"
+KERNEL_BTF="no"
+SHARE_LOG="yes"
+
+if [[ -n "${ENABLE_EXTENSIONS}" ]]; then
+	ENABLE_EXTENSIONS+=","
+fi
+ENABLE_EXTENSIONS+="ufs"
